0399d735fab7debe3e05e0f568fbf33ffa5c5068
Dmytro Dyshuk <dd.kronos@gmail.com>
2024-02-29 14:43:37 +0200

    Java version updated to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,14 +64,14 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     namespace = "com.motivation.app"
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_11.toString()
+            jvmTarget = JavaVersion.VERSION_17.toString()
         }
     }
 }
